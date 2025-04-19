618f9a02592aff22afe935d7a48446d7a90ad03c
Rohit Raj Singh <singhrohit1020029@gmail.com>
2025-04-19 21:12:19 +0530

    setup crop suggestion

--- server/model/crop.js
+++ server/model/crop.js
@@ -3,15 +3,16 @@ const mongoose = require('mongoose');
 const cropSchema = new mongoose.Schema({
   name: { type: String, required: true },
   scientificName: String,
-  description: String,
-  suitableSoilTypes: [String],
-  suitableSeasons: [String],
-  waterRequirements: String, // 'High', 'Medium', 'Low'
-  averageInvestment: Number,
-  growthDuration: Number, // in days
-  commonPests: [String],
-  images: [String],
-  suitabilityScore: { type: Number, default: 0 }
+  type:{ type: String, required: true }, // 'Vegetable', 'Fruit', 'Grain', etc.
+  description :{type:String},
+  soilTypes: [String],
+  seasons: [String],
+  water: String, // 'High', 'Medium', 'Low'
+  investment: Number,
+  duration: Number, // in days
+  pests: [String],
+  // images: [String],
+  // suitabilityScore: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('Crop', cropSchema);
--- server/routes/sellerRoutes.js
+++ server/routes/sellerRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 router = express.Router();
 const {registerSeller, loginSeller} = require('../controllers/authSellerController');
-const { route } = require('./userRoutes');
 
 router.post('/register', registerSeller);
 router.post('/login',loginSeller);
 
-router.post('/addProduct',addPoduct);
-router.post('/editProduct',editProduct);
-router.post('/deleteProduct',deleteProduct);
+// router.post('/addProduct',addPoduct);
+// router.post('/editProduct',editProduct);
+// router.post('/deleteProduct',deleteProduct);
 
 module.exports = router;
